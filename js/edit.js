a5cef5725e9bcd46270d8cf83990d7aa41122a0c
SeiLan0123 <seiran0123@gmail.com>
2020-12-28 17:03:00 +0900

    add firebase

--- js/edit.js
+++ js/edit.js
@@ -11,3 +11,18 @@ const editor = new EditorJS({
     }
 
 });
+
+function save(){
+    const storageRef = firebase.storage().ref();
+    const testImgsRef = storageRef.child("testpostjson/test.json")
+
+    editor.save().then((outputData) => {
+        const txt = JSON.stringify(outputData);
+        testImgsRef.putString(txt).then(function(snapshot) {
+            console.log('Uploaded a raw string!');
+          });
+        console.log('Article data: ', outputData)
+       }).catch((error) => {
+        console.log('Saving failed: ', error)
+       });
+}
